91a34e8339d4ebbf84e6ecdc926e3b21a9be03dc
SahilBhatti <sahilsharry27@gmail.com>
2021-12-17 19:01:56 +0530

    17 december

--- lib/pages/user/SocailLogin.dart
+++ lib/pages/user/SocailLogin.dart
@@ -1,13 +1,11 @@
 import 'package:demoapp/constants/constants.dart';
 import 'package:demoapp/pages/drawer/drawer.dart';
 import 'package:demoapp/utlis/platte.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hexcolor/hexcolor.dart';
-// import 'package:customer_app/views/loginwithfacebook/login.dart';
-
-import 'Login.dart';
 
 
 class SocailLogin extends StatefulWidget {
@@ -20,7 +18,21 @@ class SocailLogin extends StatefulWidget {
 
 class _SocailLoginState extends State<SocailLogin> {
   GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
-  // GoogleSignInAccount? user =_googleSignIn.currentUser;
+  
+  Future<UserCredential> signInWithFacebook() async {
+    // Trigger the sign-in flow
+    String userEmail = '';
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    // Create a credential from the access token
+    final OAuthCredential facebookAuthCredential =
+        FacebookAuthProvider.credential(loginResult.accessToken!.token);
+    final userData = await FacebookAuth.instance.getUserData();
+    userEmail = userData['email'];
+    Navigator.push(context, MaterialPageRoute(builder: (context)=> Home()));
+    // Once signed in, return the UserCredential
+    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+  }
   @override
   Widget build(BuildContext context) {
     return   Scaffold(
@@ -73,7 +85,8 @@ class _SocailLoginState extends State<SocailLogin> {
                                 minimumSize: Size(double.infinity,
                                     44), // double.infinity is the width and 30 is the height
                               ),
-                              onPressed: () {
+                              onPressed: ()async {
+                                signInWithFacebook();
                                 // Navigator.push(context, MaterialPageRoute(builder: (context)=> Facebook()));
                                 // validateData();
                               },
@@ -97,35 +110,7 @@ class _SocailLoginState extends State<SocailLogin> {
                                 minimumSize: Size(double.infinity,
                                     44), // double.infinity is the width and 30 is the height
                               ),
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder:
-                                            (context) =>
-                                            Login())
-                                );
-                              },
-                              child:
-                              Padding(
-                                padding: const EdgeInsets.only(top:8,bottom:8),
-                                child: Text("Signup with email".toUpperCase(), style: btnWhiteText),
-                              ),
-                            )),
-
-                            Container(
-                              margin: const EdgeInsets.only(top: 20),
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  primary:HexColor(orange),
-                                  onPrimary:Colors.black,
-                                  minimumSize:Size(double.infinity,50),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius:
-                                    BorderRadius.circular(30.0)),
-                                elevation: 8.0,
-                                ),
-                                onPressed: ()async{
+                              onPressed: ()async{
                                     await _googleSignIn.signIn();
                               setState(() {
                                 Navigator.push(
@@ -137,8 +122,39 @@ class _SocailLoginState extends State<SocailLogin> {
                                 );
                               });
                                 }, 
-                              child: Text('SignUp With Google'.toUpperCase(),style: btnWhiteText,)),
-                            )
+                              child:
+                              Padding(
+                                padding: const EdgeInsets.only(top:8,bottom:8),
+                                child: Text("Signup with email".toUpperCase(), style: btnWhiteText),
+                              ),
+                            )),
+
+                            // Container(
+                            //   margin: const EdgeInsets.only(top: 20),
+                            //   child: ElevatedButton(
+                            //     style: ElevatedButton.styleFrom(
+                            //       primary:HexColor(orange),
+                            //       onPrimary:Colors.black,
+                            //       minimumSize:Size(double.infinity,50),
+                            //       shape: RoundedRectangleBorder(
+                            //         borderRadius:
+                            //         BorderRadius.circular(30.0)),
+                            //     elevation: 8.0,
+                            //     ),
+                            //     onPressed: ()async{
+                            //         await _googleSignIn.signIn();
+                            //   setState(() {
+                            //     Navigator.push(
+                            //         context,
+                            //         MaterialPageRoute(
+                            //             builder:
+                            //                 (context) =>
+                            //                 Home())
+                            //     );
+                            //   });
+                            //     }, 
+                            //   child: Text('SignUp With Google'.toUpperCase(),style: btnWhiteText,)),
+                            // )
                             
                       ],
                     )
@@ -154,3 +170,176 @@ class _SocailLoginState extends State<SocailLogin> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// // Copyright 2013 The Flutter Authors. All rights reserved.
+// // Use of this source code is governed by a BSD-style license that can be
+// // found in the LICENSE file.
+
+// // ignore_for_file: public_member_api_docs
+
+// import 'dart:async';
+// import 'dart:convert' show json;
+
+// import "package:http/http.dart" as http;
+// import 'package:flutter/material.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+
+// GoogleSignIn _googleSignIn = GoogleSignIn(
+//   // Optional clientId
+//   // clientId: '479882132969-9i9aqik3jfjd7qhci1nqf0bm2g71rm1u.apps.googleusercontent.com',
+//   scopes: <String>[
+//     'email',
+//     'https://www.googleapis.com/auth/contacts.readonly',
+//   ],
+// );
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       title: 'Google Sign In',
+//       home: SocailLogin(),
+//     ),
+//   );
+// }
+
+// class SocailLogin extends StatefulWidget {
+//   @override
+//   State createState() => SocailLoginState();
+// }
+
+// class SocailLoginState extends State<SocailLogin> {
+//   GoogleSignInAccount? _currentUser;
+//   String _contactText = '';
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+//       setState(() {
+//         _currentUser = account;
+//       });
+//       if (_currentUser != null) {
+//         _handleGetContact(_currentUser!);
+//       }
+//     });
+//     _googleSignIn.signInSilently();
+//   }
+
+//   Future<void> _handleGetContact(GoogleSignInAccount user) async {
+//     setState(() {
+//       _contactText = "Loading contact info...";
+//     });
+//     final http.Response response = await http.get(
+//       Uri.parse('https://people.googleapis.com/v1/people/me/connections'
+//           '?requestMask.includeField=person.names'),
+//       headers: await user.authHeaders,
+//     );
+//     if (response.statusCode != 200) {
+//       setState(() {
+//         _contactText = "People API gave a ${response.statusCode} "
+//             "response. Check logs for details.";
+//       });
+//       print('People API ${response.statusCode} response: ${response.body}');
+//       return;
+//     }
+//     final Map<String, dynamic> data = json.decode(response.body);
+//     final String? namedContact = _pickFirstNamedContact(data);
+//     setState(() {
+//       if (namedContact != null) {
+//         _contactText = "I see you know $namedContact!";
+//       } else {
+//         _contactText = "No contacts to display.";
+//       }
+//     });
+//   }
+
+//   String? _pickFirstNamedContact(Map<String, dynamic> data) {
+//     final List<dynamic>? connections = data['connections'];
+//     final Map<String, dynamic>? contact = connections?.firstWhere(
+//       (dynamic contact) => contact['names'] != null,
+//       orElse: () => null,
+//     );
+//     if (contact != null) {
+//       final Map<String, dynamic>? name = contact['names'].firstWhere(
+//         (dynamic name) => name['displayName'] != null,
+//         orElse: () => null,
+//       );
+//       if (name != null) {
+//         return name['displayName'];
+//       }
+//     }
+//     return null;
+//   }
+
+//   Future<void> _handleSignIn() async {
+//     try {
+//       await _googleSignIn.signIn();
+//     } catch (error) {
+//       print(error);
+//     }
+//   }
+
+//   Future<void> _handleSignOut() => _googleSignIn.disconnect();
+
+//   Widget _buildBody() {
+//     GoogleSignInAccount? user = _currentUser;
+//     if (user != null) {
+//       return Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//         children: <Widget>[
+//           ListTile(
+//             leading: GoogleUserCircleAvatar(
+//               identity: user,
+//             ),
+//             title: Text(user.displayName ?? ''),
+//             subtitle: Text(user.email),
+//           ),
+//           const Text("Signed in successfully."),
+//           Text(_contactText),
+//           ElevatedButton(
+//             child: const Text('SIGN OUT'),
+//             onPressed: _handleSignOut,
+//           ),
+//           ElevatedButton(
+//             child: const Text('REFRESH'),
+//             onPressed: () => _handleGetContact(user),
+//           ),
+//         ],
+//       );
+//     } else {
+//       return Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//         children: <Widget>[
+//           const Text("You are not currently signed in."),
+//           ElevatedButton(
+//             child: const Text('SIGN IN'),
+//             onPressed: _handleSignIn,
+//           ),
+//         ],
+//       );
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Google Sign In'),
+//         ),
+//         body: ConstrainedBox(
+//           constraints: const BoxConstraints.expand(),
+//           child: _buildBody(),
+//         ));
+//   }
+// }
